0bbba539d7b6f68174e12193c1d6610c43f348d7
Pablo <pablito@me.com>
2023-07-03 16:00:17 +0100

    Started realtime validation for feedback

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
       <Header 
     />
       <div className="container">
+        <FeedbackForm feedback={feedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
