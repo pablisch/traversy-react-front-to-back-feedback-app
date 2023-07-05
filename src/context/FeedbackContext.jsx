1007c229c8768bd0640129f4787c7b99bccc1b9f
Pablo <pablito@me.com>
2023-07-05 20:50:08 +0100

    Added update functionality

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -21,22 +21,49 @@ export const FeedbackProvider = ({ children }) => {
     text: "I am feedback message number 3. I am considerably longer than the first two.",
   },
   ])
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  })
   
+  // delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete that item?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
+  // add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
+
+  // set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    })
+  }
+
+  // update feedback itrm
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item))
+    setFeedbackEdit({
+      item: {},
+      edit: false
+    })
+  }
   
   return <FeedbackContext.Provider value={{
     feedback: feedback,
+    feedbackEdit: feedbackEdit,
     deleteFeedback: deleteFeedback,
-    addFeedback: addFeedback
+    addFeedback: addFeedback,
+    editFeedback: editFeedback,
+    updateFeedback: updateFeedback
   }}>
     {children}
   </FeedbackContext.Provider>
